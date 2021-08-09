e2cfe7edbc29026b5e678d596adbff02506a7b7c
flowers186 <62859441+dvisionst@users.noreply.github.com>
2021-08-09 21:33:25 +0000

    simplified code by creating a double alphabet in the alphabet variable.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -9,20 +9,12 @@ def caesar(start_text,shift_amount,cipher_direction):
     encrypted_text = ''
     for char in start_text:
         letter_position = alphabet.index(char)
-        if cipher_direction == "encode":
-            if letter_position >= 25:
-                adjusted_position = letter_position + shift -26
-                encrypted_text +=alphabet[adjusted_position]
-            elif letter_position < 25:
-                letter_position +=shift
-                encrypted_text += alphabet[letter_position]
+        if cipher_direction == "encode":            
+            letter_position +=shift_amount
+            encrypted_text +=alphabet[letter_position]
         elif cipher_direction == "decode":
-            if letter_position < 0:
-                adjusted_position = letter_position -shift + 26 
-                encrypted_text += alphabet[adjusted_position]
-            elif letter_position >= 0:
-                letter_position -= shift
-                encrypted_text += alphabet[letter_position]           
+            letter_position -= shift
+            encrypted_text += alphabet[letter_position]           
     print(encrypted_text)
     
 caesar(start_text=text,shift_amount=shift,cipher_direction=direction)
