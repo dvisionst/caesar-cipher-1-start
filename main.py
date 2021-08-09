fd955ff71c7813c33abb90d3b4348ac3208ed28a
flowers186 <62859441+dvisionst@users.noreply.github.com>
2021-08-09 21:14:25 +0000

    combined the decrypt and encrypt functions into a singular function.

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-# def encrypt(text,shift):
-def encrypt(text,shift):
-    encryp = ''
-    for char in text:
-        z = alphabet.index(char) + shift
-        if z >= 25:
-            y = z -26
-            encryp += alphabet[y]
-        elif z < 25:
-            encryp +=alphabet[z]
-    print(encryp)
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-encrypt(text,shift)
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-
-# word = "civilization"
-# shift = 5
-# encryp = ''
-
-# for char in word:
-#     z = alphabet.index(char) + shift
-#     if z >= 25:
-#         y = z -26
-#         encryp += alphabet[y]
-#     elif z < 25:
-#         encryp +=alphabet[z]
-
-
-
+def caesar(start_text,shift_amount,cipher_direction):
+    encrypted_text = ''
+    for char in start_text:
+        letter_position = alphabet.index(char)
+        if cipher_direction == "encode":
+            if letter_position >= 25:
+                adjusted_position = letter_position + shift -26
+                encrypted_text +=alphabet[adjusted_position]
+            elif letter_position < 25:
+                letter_position +=shift
+                encrypted_text += alphabet[letter_position]
+        elif cipher_direction == "decode":
+            if letter_position < 0:
+                adjusted_position = letter_position -shift + 26 
+                encrypted_text += alphabet[adjusted_position]
+            elif letter_position >= 0:
+                letter_position -= shift
+                encrypted_text += alphabet[letter_position]           
+    print(encrypted_text)
+    
+caesar(start_text=text,shift_amount=shift,cipher_direction=direction)
 
